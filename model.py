10a3618bf18e7ed1a9b21aecdd195b495c56c912
Dante Gates <dgates@O2O-PA-DGATES.local>
2018-02-01 15:15:15 -0500

    basic encoder structure finished

--- model.py
+++ model.py
@@ -1,6 +1,10 @@
 import numpy as np
 from keras import backend as K
+from keras import activations
 from keras.engine.topology import Layer
+from keras.initializers import RandomNormal
+from keras.layers import Embedding, Input, Dense
+
 
 # https://arxiv.org/pdf/1706.03762.pdf
 ## Enccoder
@@ -13,80 +17,109 @@ from keras.engine.topology import Layer
 # 4. Feed Forward
 # 5. Add & Norm
 
-class Attention(Layer):
-    def __init__(self, d_k, d_v, **kwargs):
-        self.d_k = d_k
-        self.d_v = d_v
-        self.attention_scalar = K.variable(np.sqrt(d_k))
-        super(Attention, self).__init__(**kwargs)
-        
-    def build(self, input_shape):
-        # perhaps these assertions are not very generic
-        # leave in place while experimenting however
-        assert input_shape[0] == self.d_k
-        assert input_shape[0] == self.d_v
-        self.K = self.add_weight(name='K',
-                                 shape=(input_shape[0], self.d_k),
-                                 initializer='uniform',
-                                 trainable=True)
-        self.V = self.add_weight(name='V',
-                                 shape=(input_shape[0], self.d_v),
-                                 initializer='uniform',
-                                 trainable=True)
-        super(Attention, self).build(input_shape)
-    
-    def call(self, Q):
-        X = self.activation(K.dot(Q, K.transpose(self.K)))
-        X /= self.d_k
-        return K.dot(X, self.V)
-    
-    def compute_output_shape(self, input_shape):
-        return (self.input_shape[0], self.d_v)
-    
+
+DEBUG = True
+def debug(*args, **kwargs):
+    if DEBUG:
+        print(*args, **kwargs)
+
 
 class MultiHeadAttention(Layer):
-    def __init__(self, *, n_heads, d_head, **kwargs):
-        self.n_heads = n_heads
-        self.d_k = self.d_v = d_head
-        self.scalar =  np.sqrt(self.d_k)
+    def __init__(self, *, h, d_model, **kwargs):
+        # activation = comparison
+        debug('init MultiHeadAttention')
+        self.h = h
+        self.d_model = d_model
+        assert self.d_model % h == 0, 'h must divide d_model evenly'
+        self.d_k = self.d_v = int(self.d_model / h)
         super().__init__(**kwargs)
         
     def build(self, input_shape):
-
+        debug('building MultiAttentionHead')
+        self.k = self.add_weight(name='k', 
+                                 shape=(1, self.d_model),
+                                 initializer='uniform',
+                                 trainable=True)
+        self.v = self.add_weight(name='v', 
+                                 shape=(1, self.d_model),
+                                 initializer='uniform',
+                                 trainable=True)
+        self.W_o = self.add_weight(name='W_o', 
+                                   shape=(self.h*self.d_v, self.d_model),
+                                   initializer='uniform',
+                                   trainable=True)
+        self.heads = [AttentionHead(self.d_model, self.d_k, self.d_v, activation='softmax')
+                      for _ in range(self.h)]
+        # _ = [head.build((self.d_model, self.d_k)) for head in self.heads]
         super().build(input_shape)
     
-    def call(self, Q):
-        return K.concatenate(self.heads)
-
-    def init_head(self):
-        pass
-
-    def attention(self, Q, K, V):
-        X = self.activation(K.dot(Q, K.transpose(K)))
-        X /= self.scalar
-        return K.dot(X, V)
+    def call(self, q):
+        concat = K.concatenate([head(q, k=self.k, v=self.v) for head in self.heads])
+        debug('concat shape', K.int_shape(concat))
+        return K.dot(concat, self.W_o)
 
 
-class Encoder:
-    pass
+class AttentionHead(Layer):
+    def __init__(self, d_model, d_k, d_v, activation, **kwargs):
+        debug('init AttentionHead') 
+        self.d_model = d_model
+        self.d_k = d_k
+        self.d_v = d_v
+        self.scalar = np.sqrt(self.d_k)
+        self.activation = activations.get(activation)
+        super().__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.W_q = self.add_weight(name='W_q',
+                                   shape=(self.d_model, self.d_k),
+                                   initializer=RandomNormal(mean=0.0, stddev=1.0),
+                                   trainable=True)
+        self.W_k = self.add_weight(name='W_k',
+                                   shape=(self.d_model, self.d_k),
+                                   initializer=RandomNormal(mean=0.0, stddev=1.0),
+                                   trainable=True)
+        self.W_v = self.add_weight(name='W_v',
+                                   shape=(self.d_model, self.d_k),
+                                   initializer=RandomNormal(mean=0.0, stddev=1.0),
+                                   trainable=True)
+        super().build(input_shape)
+
+    def call(self, q, k, v):
+        q_proj = K.dot(q, self.W_q)
+        k_proj = K.dot(k, self.W_k)
+        v_proj = K.dot(v, self.W_v)
+        value_weights = \
+            self.activation(K.dot(q_proj, K.transpose(k_proj)) / self.scalar)
+        debug('value weights shape', K.int_shape(value_weights))
+        return K.dot(value_weights, v_proj)
 
 
-class Decoder:
-    pass
+class LayerNorm(Layer):
+    def __init__(self, x, **kwargs):
+        super().__init__(**kwargs)
+        self.x = x
+        self.supports_masking = True
 
+    def call(self, inputs):
+        return self.x + inputs
 
-# this is the Transformer from paper
-class Model:
-    pass
-
+    def compute_output_shape(self, input_shape):
+        return input_shape
 
 if __name__ == '__main__':
     h = 8
-    d_k = 64
-    d_v = 64
     # this is so cocnat(heads) has shape d_model
     # (as each attention output has shape d_v)
+    N = 6
     d_model = 64 * h
+    vocab_size = 30
+    DEBUG = False
 
-    attention_layers = [Attention(d_k=d_k, d_v=d_v, activation='softmax') for _ in range(h)]
-    multi_head = MultiHeadAttention(attention_layers)
+    input_ = Input(shape=(None,))
+    embedding = Embedding(30, d_model)(input_)
+    layer_norm = LayerNorm(embedding)
+    for _ in range(N):
+        encoder = MultiHeadAttention(h=h, d_model=d_model)(embedding)
+        encoder = layer_norm(encoder)
+        encoder = Dense(d_model)(encoder)
+        encoder = layer_norm(encoder)
