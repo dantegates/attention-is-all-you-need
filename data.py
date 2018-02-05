7cd3bc73ce65ec4db919854bb8595b5de128ecb2
dantegates <dantegates@hotmail.com>
2018-02-05 14:50:20 -0500

    initial commit

---
+++ data.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+
+text = """Boy, you gotta carry that weight
+Carry that weight a long time
+Boy, you gonna carry that weight
+Carry that weight a long time
+
+I never give you my pillow
+I only send you my invitation
+And in the middle of the celebrations
+I break down
+
+Boy, you gotta carry that weight
+Carry that weight a long time
+Boy, you gotta carry that weight
+You're gonna carry that weight along time
+"""
+
+
+def training_data(max_len):
+    chars = sorted(set(text))
+    vocab_size = len(chars)
+    char_indices = dict((c, i) for i, c in enumerate(chars))
+    indices_char = dict((i, c) for i, c in enumerate(chars))
+    sentences = []
+    next_chars = []
+    for i in range(0, len(text) - max_len):
+        sentences.append(text[i:i+max_len])
+        next_chars.append(text[i+1:max_len+1])
+    x = np.zeros((len(sentences), max_len), dtype=np.int64)
+    y = np.zeros((len(sentences), max_len, vocab_size+1), dtype=np.int64)
+    for i, s in enumerate(sentences):
+        for t, char in enumerate(s):
+            x[i, t] = char_indices[char]
+            y[i, t, char_indices[char]] = 1
+    # offset training/test
+    x = x[:-1]
+    y = y[1:]
+    return x, y, vocab_size
