8a04f32e95fbf968e044d2c5264aa29c738c89fc
dantegates <dantegates@hotmail.com>
2018-02-06 16:04:35 -0500

    start generating text from start

--- testing.py
+++ testing.py
@@ -1,16 +1,16 @@
 import numpy as np
-from data import BEATLES
+from data import BEATLES, CNN
 from keras.callbacks import LambdaCallback, LearningRateScheduler
 from model import Transformer
 
 n_heads = 8
-encoder_layers = decoder_layers = 2
+encoder_layers = decoder_layers = 4
 d_model = 64 * n_heads
-sequence_len = 10
-warmup_steps = 40
+sequence_len = 100
+warmup_steps = 100
+batch_size = 120
 
-training_data = BEATLES(max_len=sequence_len)
-x, y = training_data.x, training_data.y
+training_data = CNN(max_len=sequence_len, batch_size=batch_size)
 vocab_size = training_data.vocab_size + 1
 
 model = Transformer(
@@ -18,20 +18,24 @@ model = Transformer(
         d_model=d_model, vocab_size=vocab_size, sequence_len=sequence_len)
 
 
-def generate_text(epoch, logs):
+def generate_text(epoch, logs, mode='random'):
     if epoch % 10 > 0:
         return
     i = np.random.randint(len(x))
-    xi = x[i].reshape((1, -1))
+    x = np.zeros((1, sequence_len))
     terminate = training_data.char_map[training_data.TERMINATE]
     next_idx = -1
-    text = ''.join(training_data.idx_map[i] for i in xi[0])
+    text = ''.join(training_data.idx_map[i] for i in x[0])
+    print('\nusing seed', repr(text))
     while next_idx != terminate and len(text) < 100:
-        pred = model.predict([xi, xi])
+        pred = model.predict([x, x])
         probs = pred[0][-1]
         next_idx = np.random.choice(range(len(probs)), p=probs)
         text += training_data.idx_map[next_idx]
-    print(text)
+        # shift elements backward
+        x = np.roll(x, -1)
+        x[0, -1] = next_idx
+    print(repr(text))
 
 def lr_schedule(epoch):
     epoch += 1
@@ -42,6 +46,9 @@ callbacks = []
 callbacks.append(LambdaCallback(on_epoch_end=generate_text))
 callbacks.append(LearningRateScheduler(lr_schedule))
 
+model.summary(line_length=100)
 model.compile(loss='categorical_crossentropy', optimizer='adam')
-model.fit([x, x], y, batch_size=30, epochs=100, callbacks=callbacks)
+gen = iter(training_data)
+model.fit_generator(gen, steps_per_epoch=len(training_data.files),
+                    epochs=100, callbacks=callbacks)
 model.save('model.h5')
