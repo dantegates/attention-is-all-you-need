dffdf2f9d35d2a113c03c3514d4498357bb56551
dantegates <dantegates@hotmail.com>
2018-02-06 12:00:37 -0500

    added callback to generate text during train

--- testing.py
+++ testing.py
@@ -1,19 +1,41 @@
+from keras.callbacks import LambdaCallback
+import numpy as np
+
 from model import Transformer
-from data import training_data
+from data import BEATLES
 
 
 n_heads = 8
-encoder_layers = decoder_layers = 1
+encoder_layers = decoder_layers = 8
 d_model = 64 * n_heads
 sequence_len = 15
 
-x, y, vocab_size = training_data(sequence_len)
-vocab_size = vocab_size + 1
+training_data = BEATLES(max_len=sequence_len)
+x, y = training_data.x, training_data.y
+vocab_size = training_data.vocab_size + 1
 
 model = Transformer(
         n_heads=n_heads, encoder_layers=encoder_layers, decoder_layers=decoder_layers,
         d_model=d_model, vocab_size=vocab_size, sequence_len=sequence_len)
 
+
+def generate_text(epoch, logs):
+    if epoch % 10 > 0:
+        return
+    xi = x[0].reshape((1, -1))
+    terminate = training_data.char_map[training_data.TERMINATE]
+    next_idx = -1
+    text = ''
+    i = 0
+    while next_idx != terminate and i < 100:
+        pred = model.predict([xi, xi])
+        probs = pred[0][-1]
+        next_idx = np.random.choice(range(len(probs)), p=probs)
+        text += training_data.idx_map[next_idx]
+        i += 1
+    print(text)
+
+cb = LambdaCallback(on_epoch_end=generate_text)
 model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
-model.fit([x, x], y, batch_size=30, epochs=10)
+model.fit([x, x], y, batch_size=30, epochs=100, callbacks=[cb])
 model.save('model.h5')
