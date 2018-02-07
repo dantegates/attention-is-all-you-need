5d38c43d1ca07dfbc18ed13806defee8df1267c2
dante <dantegates@hotmail.com>
2018-02-07 10:06:46 -0500

    renamed

--- train.py
+++ train.py
@@ -1,86 +1,54 @@
-import fnmatch
-import os
-import sys
-
 import numpy as np
-from sklearn.feature_extraction import DictVectorizer
-from model import init_model
+from data import BEATLES, CNN
+from keras.callbacks import LambdaCallback, LearningRateScheduler
+from model import Transformer
+
+n_heads = 8
+encoder_layers = decoder_layers = 4
+d_model = 64 * n_heads
+sequence_len = 100
+warmup_steps = 100
+batch_size = 30
+
+training_data = CNN(max_len=sequence_len, batch_size=batch_size)
+vocab_size = training_data.vocab_size + 1
+
+model = Transformer(
+        n_heads=n_heads, encoder_layers=encoder_layers, decoder_layers=decoder_layers,
+        d_model=d_model, vocab_size=vocab_size, sequence_len=sequence_len)
 
 
-CONTEXT_LENGTH = 200
-BATCH_SIZE = 100
-EPOCHS = 1
-DEBUG = False
+def generate_text(epoch, logs, mode='random'):
+    # if epoch % 10 > 0:
+    #     return
+    i = np.random.randint(vocab_size)
+    x = np.zeros((1, sequence_len))
+    terminate = training_data.char_map[training_data.TERMINATE]
+    next_idx = -1
+    text = ''.join(training_data.idx_map[i] for i in x[0])
+    print('\nusing seed', repr(text))
+    while next_idx != terminate and len(text) < 5000:
+        pred = model.predict([x, x])
+        probs = pred[0][-1]
+        next_idx = np.random.choice(range(len(probs)), p=probs)
+        text += training_data.idx_map[next_idx]
+        # shift elements backward
+        x = np.roll(x, -1)
+        x[0, -1] = next_idx
+    print(repr(text))
 
+def lr_schedule(epoch):
+    epoch += 1
+    lr = d_model**-.5 * min(epoch**-.5, epoch*warmup_steps**-1.5)
+    return lr
 
-IGNORE_PATHS = ['site-packages', os.getcwd(), 'mac', 'bin']
-PY_FILES = []
-SEARCH_PATHS = filter(lambda p: not any(ignore in p for ignore in IGNORE_PATHS), sys.path)
-for path in SEARCH_PATHS:
-    if DEBUG:
-        print('searching in', path)
-    for root, dirnames, filenames in os.walk(path):
-        if not 'site-package' in root:
-            for filename in fnmatch.filter(filenames, '*.py'):
-                dest = os.path.join(root, filename)
-                PY_FILES.append(dest)
+callbacks = []
+callbacks.append(LambdaCallback(on_epoch_end=generate_text))
+callbacks.append(LearningRateScheduler(lr_schedule))
 
-
-class Batcher:
-    def __init__(self, context_size, step_size, files):
-        self.context_size = context_size
-        self.files = files
-        self.step_size = step_size
-        charset = set(self.gen_corpus(files))
-        self.charset_size = len(charset)
-        self._dv = DictVectorizer(sparse=False)
-        self._dv.fit([{'c': c} for c in charset])
-
-    def __iter__(self):
-        for file in self.files:
-            content = self.gen_text(file)
-            if len(content) < self.context_size:
-                continue
-            V = self._dv.transform([{'c': c} for c in content])
-            n_samples = len(content) - self.context_size
-            X = np.zeros((n_samples, self.context_size, self.charset_size))
-            y = np.zeros((n_samples, self.charset_size))
-            for i in range(0, n_samples, self.step_size):
-                X[i] = V[i:i+self.context_size]
-                y[i] = V[i+self.context_size]
-            yield X, y
-
-    def gen_text(self, file):
-        with open(file) as f:
-            out = []
-            try:
-                contents = f.read()
-            except UnicodeDecodeError:
-                if DEBUG:
-                    print('ignoring', file, 'unicode error')
-            else:
-                out = ['<start>' for _ in range(CONTEXT_LENGTH)]
-                out += list(contents)
-                out.append('<end>')
-            return out
-
-    def gen_corpus(self, files):
-        return (c for f in files
-                  for text in self.gen_text(f)
-                  for c in text)
-
-    @property
-    def char_map(self):
-        return {v: k.partition('c=')[-1] for k, v in self._dv.vocabulary_.items()}
-
-
-if __name__ == '__main__':
-    batcher = Batcher(CONTEXT_LENGTH, 1, PY_FILES[:100])
-    train = list(batcher)
-    Xs, ys = zip(*train)
-    X = np.concatenate(Xs)
-    y = np.concatenate(ys)
-
-    encoder, decoder = init_model(h=8, encoder_layers=6, decoder_layers=6)
-    decoder.compile(loss='categorical_crossentropy', optimizer='adam')
-    decoder.fit(X, y, batch_size=BATCH_SIZE, epochs=EPOCHS)
+model.summary(line_length=100)
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+gen = iter(training_data)
+model.fit_generator(gen, steps_per_epoch=100,#len(training_data.files),
+                    epochs=100, callbacks=callbacks, verbose=2)
+model.save('model.h5')
