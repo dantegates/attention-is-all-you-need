0884aa73a7d7a03c6a32671cb62b28a8433b27b1
dantegates <dantegates@hotmail.com>
2018-02-07 10:14:49 -0500

    fixed batching

--- data.py
+++ data.py
@@ -13,14 +13,36 @@ class TrainingData:
     TERMINATE = '<end>'
     UNKOWN = '<unk>'
 
-    def __init__(self, text, max_len):
-        self.text = text
-        self.max_len = max_len
-        self.tokens = [self.BEGIN]*max_len + list(text) + [self.TERMINATE]
-        self.chars = sorted(set(text)) + {self.BEGIN} + {self.TERMINATE} + {self.UNKOWN}
+    def __init__(self, directory, extension, sequence_len, batch_size):
+        self.sequence_len = sequence_len
+        self.chars = sorted(set(string.printable))
         self.vocab_size = len(self.chars)
         self.char_map, self.idx_map = self.init_maps()
-        self.x, self.y = self.init_xy(text)
+        self.directory = directory
+        self.files = glob.glob(
+            os.path.join(self.directory, '*%s' % extension))
+        self.batch_size = batch_size
+        self.skipped = set()
+
+    def __iter__(self):
+        while True:
+            random.shuffle(self.files)
+            for file in self.files:
+                with open(file) as f:
+                    content = f.read()
+                    if len(content) < self.sequence_len+self.batch_size:
+                        if not file in self.skipped:
+                            print('skipping file', file)
+                            self.skipped.add(file)
+                        continue
+                    else:
+                        content = [self.BEGIN]*self.sequence_len + list(content) + [self.TERMINATE]
+                    i = random.randint(0, len(content) - self.sequence_len)
+                    # kind of sloppy, just don't feel like writing batching
+                    # correctly at the moment
+                    content = content[i:i+self.sequence_len+self.batch_size]
+                    x, y = self.init_xy(content)
+                    yield [x, x], y
 
     def init_maps(self):
         char_map, idx_map = {}, {}
@@ -38,12 +60,11 @@ class TrainingData:
     def init_xy(self, text):
         sentences = []
         next_sentences = []
-        text = [self.BEGIN]*self.max_len + list(text) + [self.TERMINATE]
-        for i in range(0, len(text)-self.max_len):
-            sentences.append(text[i:i+self.max_len])
-            next_sentences.append(text[i+1:self.max_len+1])
-        x = np.zeros((len(sentences), self.max_len), dtype=np.int64)
-        y = np.zeros((len(sentences), self.max_len, self.vocab_size+1))
+        for i in range(0, len(text)-self.sequence_len):
+            sentences.append(text[i:i+self.sequence_len])
+            next_sentences.append(text[i+1:self.sequence_len+1])
+        x = np.zeros((len(sentences), self.sequence_len), dtype=np.int64)
+        y = np.zeros((len(sentences), self.sequence_len, self.vocab_size+1))
         for i, s in enumerate(sentences):
             for t, char in enumerate(s):
                 char = char if char in self.chars else self.UNKOWN
@@ -55,39 +76,5 @@ class TrainingData:
         y = y[1:]
         return x, y
 
-
-class FileGeneratorTrainingData(TrainingData):
-    def __init__(self, max_len, directory, extension, batch_size):
-        self.max_len = max_len
-        self.chars = sorted(set(string.printable))
-        self.vocab_size = len(self.chars)
-        self.char_map, self.idx_map = self.init_maps()
-        self.directory = directory
-        self.files = glob.glob(
-            os.path.join(self.directory, '*%s' % extension))
-        self.batch_size = batch_size
-        self.skipped = set()
-
-    def __iter__(self):
-        while True:
-            random.shuffle(self.files)
-            for file in self.files:
-                with open(file) as f:
-                    content = f.read()
-                    if len(content) < self.max_len+self.batch_size:
-                        if not file in self.skipped:
-                            print('skipping file', file)
-                            self.skipped.add(file)
-                        continue 
-                    i = random.randint(0, len(content) - self.max_len)
-                    # sloppy hack as below
-                    content = content[i:i+self.max_len]
-                    x, y = self.init_xy(content)
-                    # kind of sloppy, just don't feel like writing batching
-                    # correctly at the moment
-                    idx = np.random.randint(len(x), size=self.batch_size)
-                    yield [x[idx], x[idx]], y[idx]
-
-
-BEATLES = partial(FileGeneratorTrainingData, directory='beatles', extension='.txt')
-CNN = partial(FileGeneratorTrainingData, directory='cnn/**', extension='.story')
+BEATLES = partial(TrainingData, directory='beatles', extension='.txt')
+CNN = partial(TrainingData, directory='cnn/**', extension='.story')
