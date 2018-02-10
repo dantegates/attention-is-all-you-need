5d6ada97915a219a399edf6e376905daf1e68cb5
dantegates <dantegates@hotmail.com>
2018-02-10 11:07:12 -0500

    fixed tokenization around newlines

--- data.py
+++ data.py
@@ -93,8 +93,9 @@ class BatchGenerator:
         if self.tokenizer == 'chars':
             tokens = list(text)
         elif self.tokenizer == 'words':
-            for p in string.punctuation:
+            for p in string.punctuation.replace("'", ''):
                 text = text.replace(p, ' %s ' % p)
+            text = text.replace('\n', ' \n ')
             tokens = [s.strip() for s in text.split(' ')]
             tokens = list(itertools.chain(tokens))
         else:
