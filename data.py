8ec6ab5742f033727271ee8fa77edf830d775b44
dantegates <dantegates@hotmail.com>
2018-02-09 19:40:44 -0500

    vastly improved tokenizer

--- data.py
+++ data.py
@@ -1,66 +1,71 @@
 import collections
-from functools import partial
 import glob
+import logging
 import os
+import random
 import string
+import itertools
+from functools import partial
 
 import numpy as np
 
+logger = logging.getLogger(__name__)
+
 
 class BatchGenerator:
-
     START = '<start>'
-    END = '<end>'
+    END = '\a'  # this needs to be a single ascii character to make
+                # tokenizing simple
     UNKOWN = '<unk>'
-    CHARS = sorted(set(string.printable))
-
-    CHAR_MAP = {
-        START: 0,
-        END: 1,
-        UNKOWN: 2,
-    }
-
-    IDX_MAP = {i: c for c, i in CHAR_MAP.items()}
-
-
-    CHAR_MAP.update((c, i) for i, c in enumerate(CHARS, start=max(IDX_MAP+1))
-    IDX_MAP.update((i, c) for i, c in enumerate(CHARS, start=max(IDX_MAP+1)))
-
-    VOCAB_SIZE = len(CHARS)
 
     def __init__(self, encoder_len, decoder_len, directory, extension, batch_size,
                  step_size, tokenizer='chars'):
         self.encoder_len = encoder_len
         self.decoder_len = decoder_len
         self.directory = directory
-        self.files = glob.glob(os.path.join(self.directory, '*%s' % extension))
+        self.files = glob.glob(os.path.join(self.directory, '*%s' % extension))[:2]
         self.batch_size = batch_size
         self.step_size = step_size
-        self.n_batches = sum(1 for _ in self.fetch_examples()) // self.batch_size
         self.tokenizer = tokenizer
+
+        self.examples = self.fetch_examples()
+        self.n_batches = (len(self.examples)-1) // self.batch_size
+        self.tokens = self.init_tokens()
+
+        self.char_map = {
+            self.START: 0,
+            self.END: 1,
+            self.UNKOWN: 2,
+        }
+        self.idx_map = {i: c for c, i in self.char_map.items()}
+        self.char_map.update((c, i) for i, c in enumerate(self.tokens, start=max(self.idx_map)+1))
+        self.idx_map.update((i, c) for i, c in enumerate(self.tokens, start=max(self.idx_map)+1))
+        self.vocab_size = len(self.char_map)
+
         self.skipped = set()
 
     def __iter__(self):
         while True:
+            random.shuffle(self.examples)
             x1 = np.zeros((self.batch_size+1, self.encoder_len))
             x2 = np.zeros((self.batch_size+1, self.decoder_len))
-            y = np.zeros((self.batch_size+1, self.decoder_len, self.VOCAB_SIZE+1))
-            for i, (x1_content, x2_content) in enumerate(self.fetch_examples()):
+            y = np.zeros((self.batch_size+1, self.decoder_len, self.vocab_size+1))
+            for i, (context, target) in enumerate(self.examples):
                 i = i % self.batch_size
-                x1[i, :] = self.make_x(x1_content[-self.encoder_len:], encoder=True)
-                x2[i, :] = self.make_x(x2_content, encoder=False)
-                y[i+1,:,:] = self.make_y(x2_content)
+                x1[i, :] = self.tokens_to_x(context)
+                x2[i, :] = self.tokens_to_x(target)
+                y[i+1,:,:] = self.tokens_to_y(target)
                 if i == self.batch_size-1:
                     # offset x/y
                     yield [x1[:-1], x2[:-1]], y[1:]
                     x1 = np.zeros((self.batch_size+1, self.encoder_len))
                     x2 = np.zeros((self.batch_size+1, self.decoder_len))
-                    y = np.zeros((self.batch_size+1, self.decoder_len, self.VOCAB_SIZE+1))
+                    y = np.zeros((self.batch_size+1, self.decoder_len, self.vocab_size+1))
 
     def fetch_content(self):
         for file in self.files:
             with open(file) as f:
-                content = f.read()
+                content = f.read() + self.END
                 if len(content) < self.batch_size:
                     if not file in self.skipped:
                         print('skipping', file)
@@ -68,46 +73,64 @@ class BatchGenerator:
                 yield content
 
     def fetch_examples(self):
+        examples = []
         for content in self.fetch_content():
             lines = content.split('\n')
             for i in range(len(lines)):
-                l1, l2 = lines[:i], lines[i]
-                for j in range(0, len(l2)+1, self.step_size):
-                    yield '\n'.join(l1), l2[:j] + '\n'
+                context_text, target_text = '\n'.join(lines[:i]), lines[i] + '\n'
+                context_tokens = self.tokenize(context_text)
+                context = context_tokens[-self.encoder_len:]
+                target_tokens = self.tokenize(target_text)
+                for j in range(1, len(target_tokens)+1, self.step_size):
+                    target = target_tokens[:j]
+                    logger.debug('fetched context: %r', context)
+                    logger.debug('feteched target: %r', target)
+                    examples.append((context_tokens, target_tokens))
+        return examples
 
     def tokenize(self, text):
         tokens = []
         if self.tokenizer == 'chars':
             tokens = list(text)
-        # if self.tokenizer == 'words':
-        #     for p in string.punctuation:
-        #         text = text.replace(p, ' %s ' % p)
-        #     tokens = [s.strip() for s in text.split()]
-        tokens = [c if c in self.CHARS else self.UNKOWN
-                  for c in tokens]
-        return collections.deque(tokens)
+        elif self.tokenizer == 'words':
+            for p in string.punctuation:
+                text = text.replace(p, ' %s ' % p)
+            tokens = [s.strip() for s in text.split(' ')]
+            tokens = list(itertools.chain(tokens))
+        else:
+            raise ValueError('unrecognized tokenizer')
+        return tokens
 
-    def make_x(self, text, encoder):
-        sequence_len = self.encoder_len if encoder else self.decoder_len
-        tokens = self.tokenize(text)
-        if len(tokens) < sequence_len:
-            tokens.append(self.END)
+    def tokens_to_x(self, tokens):
+        tokens = collections.deque(tokens)
+        sequence_len = self.encoder_len
         while len(tokens) < sequence_len:
             tokens.appendleft(self.START)
-        x = np.array([self.CHAR_MAP[c] for c in tokens])
+        logger.debug('x tokens: %r', tokens)
+        x = np.array([self.char_map[c] for c in tokens])
         return x
 
-    def make_y(self, text):
+    def tokens_to_y(self, tokens):
+        tokens = collections.deque(tokens)
         sequence_len = self.decoder_len
-        tokens = self.tokenize(text)
-        if len(tokens) < sequence_len:
-            tokens.append(self.END)
         while len(tokens) < sequence_len:
             tokens.appendleft(self.START)
-        y = np.zeros((sequence_len, self.VOCAB_SIZE+1))
+        y = np.zeros((sequence_len, self.vocab_size+1))
+        logger.debug('y tokens: %r', tokens)
         for i, c in enumerate(tokens):
-            y[i][self.CHAR_MAP[c]] = 1
+            idx = self.char_map[c]
+            y[i][idx] = 1
         return y
+
+    def idx_to_char(self, idx):
+        return self.idx_map[idx] if idx in self.idx_map else self.UNKOWN
+
+    def init_tokens(self):
+        tokens = set()
+        for context, target in self.examples:
+            tokens.update(context)
+            tokens.update(target)
+        return sorted(tokens)
 
 
 BEATLES = partial(BatchGenerator, directory='beatles', extension='.txt')
